09e712d571b0f51fe0287f0df891147615c9958d
gilbertateka <gilbert.ateka@aimsoft.co.ke>
2023-06-04 18:42:18 +0300

    delete email

--- controller/emailCtrl.js
+++ controller/emailCtrl.js
@@ -6,8 +6,8 @@ const sendEmail = asyncHandler(
         let transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-              user: "fdfdfdfd",
-              pass: "vdyxgaxrcvlofhvx",
+              user: process.env.EMAIL,
+              pass: process.env.EMAIL_PASS,
             },
           });
           
